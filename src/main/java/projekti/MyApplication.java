fe1ec0415dd2cf312ef8b0ba2008690043453cc1
Kim Jämsen <jamsen.kim@gmail.com>
2019-05-05 01:31:12 +0300

    fixed timeZone

--- src/main/java/projekti/MyApplication.java
+++ src/main/java/projekti/MyApplication.java
@@ -1,10 +1,17 @@
 package projekti;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Helsinki"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class);
